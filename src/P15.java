8595e2ba66948da462ad31e25193d5f2f8bb17f8
LEE <LEE@DESKTOP-7TN5DVA>
2021-03-24 17:49:09 +0900

    Commit Message

--- src/P15.java
+++ src/P15.java
@@ -17,7 +17,7 @@ public class P15 {
 				b=i;
 			}
 		}
-		System.out.println("57에 가장 가까운 수: "+arr[b]);
+		System.out.println("57에 가장 가까운 수: "+ arr[b]);
 		
 //문2: 1~100, 4의배수 개수, 값 출력
 		
@@ -31,6 +31,18 @@ public class P15 {
 			}
 		}
 		System.out.println("개수 : "+count+"  합 : "+ sum);
+		
+//문3: 54321초가 몇시간 몇분 몇초인가?
+		int h,m,s;
+		int tt=54321;
+		
+		m = tt / 60;
+		s = tt % m;
+		h = m / 60;
+		m = m - 60*h;
+
+		
+		System.out.println(h+"시간 "+m+ "분 "+s+"초");
 
 	}
 }
